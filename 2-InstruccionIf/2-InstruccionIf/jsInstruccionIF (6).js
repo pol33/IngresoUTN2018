1a5fc06f0dba11a51d61295f221b089cf4c30ed8
pol33 <36174687+pol33@users.noreply.github.com>
2018-02-18 14:36:12 -0300

    Add files via upload

--- 2-InstruccionIf/2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,8 +1,20 @@
 function Mostrar()
 {
 //tomo la edad  
+edad=parseInt(document.getElementById("edad").value);
 
-
+if( edad < 13 )
+{
+    alert("Niño");
+}
+else if( edad < 18 )
+{
+    alert("Adolescente");
+}
+else ( edad > 17 )
+{
+    alert("Mayor de edad");
+}
 
 
 }//FIN DE LA FUNCIÓN
